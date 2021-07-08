6bdc24ac4c4f6257cbced69c5deaef212e504885
Isadora Kondo <kondoiqcdev@gmail.com>
2021-07-08 12:17:38 -0300

    Execução do exercício

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace peso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char sexo;
+            double alt=0;
+            double result=0;
+
+            Console.WriteLine("+--------------+");
+            Console.WriteLine("|Seu peso ideal|");
+            Console.WriteLine("+--------------+");
+
+            Console.Write("Digite sua altura em metros: ");
+            alt = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Informe seu sexo utilizando (M) para masculino ou (F) para feminino: ");
+            sexo=Convert.ToChar(Console.ReadLine());
+
+            if(sexo == 'm'){
+                result = (alt * 72.7) -58.0;
+                Console.WriteLine($"Seu peso ideal é {result}kg");
+            }
+            if(sexo == 'f'){
+                  result = (alt * 62.1)- 44.7;
+                  Console.WriteLine($"Seu peso ideal é {result}kg");
+            }
+
+        }
+    }
+}
